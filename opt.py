f7c17c60d8c004ab1d1bbb8cdae4a971e8407f5a
Sijie Kong <sijie_kong@ucsb.edu>
2025-03-08 19:31:53 +0000

    Implemented is_sparse

--- opt.py
+++ opt.py
@@ -8,7 +8,8 @@ def optimize(
     l: float = 0.1,
     lr: float = 1e-3,
     max_iter: int = 1000,
-    device: str = "cuda"
+    device: str = "cuda",
+    is_sparse: bool = False
 ) -> np.ndarray:
     """
     minimize ||C - AA^TX||_F^2 + l||A||_1
@@ -18,12 +19,15 @@ def optimize(
     X: torch.Tensor = torch.tensor(X, dtype=torch.float64, device=device)
     A: torch.Tensor = torch.randn(C.shape[0], X.shape[0], dtype=torch.float64, requires_grad=True, device=device)
     C: torch.Tensor = torch.tensor(C, dtype=torch.float64, device=device)
+    if is_sparse:
+        X = X.to_sparse()
 
     optimizer = torch.optim.Adam([A], lr=lr)
 
     for i in range(max_iter):
         optimizer.zero_grad()
-        loss = torch.norm(C - A @ A.T @ X, p="fro")**2 + l * torch.sum(torch.norm(A, p=1, dim=1))
+        AAX = torch.sparse.mm(A @ A.T, X) if is_sparse else A @ A.T @ X
+        loss = torch.norm(C - AAX, p="fro")**2 + l * torch.sum(torch.norm(A, p=1, dim=1))
         loss.backward()
         print(f"Iteration {i + 1}, Loss: {loss.item()}")
         optimizer.step()
